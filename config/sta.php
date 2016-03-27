def587ceab72c4c59b5dae24f6b99c53332d86a4
Jose Manuel García Maleno <jmgarciamaleno@gmail.com>
2016-03-27 20:57:27 +0200

    Remove staging entry point and configuration files.

--- config/sta.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// RabbitMQ server configuration.
-$app['queue_host'] = '';
-$app['queue_port'] = null; // Default 5672.
-$app['queue_user'] = '';
-$app['queue_pass'] = '';
-$app['queue_name'] = '';
